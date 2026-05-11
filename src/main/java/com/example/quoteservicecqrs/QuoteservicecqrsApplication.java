721d89e984921fea0ccb186f7a986437969f8485
Tri Nguyen <tri.nguyen@alpaca.vn>
2026-05-11 10:32:13 +0700

    init

---
+++ src/main/java/com/example/quoteservicecqrs/QuoteservicecqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.quoteservicecqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteservicecqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteservicecqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quoteservicecqrs/QuoteservicecqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quoteservicecqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteservicecqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
